f906475aaeed7d1864dbdc92ff512bda4c419b00
spacelinkers <josen221@gmail.com>
2018-11-12 12:59:04 +0600

    simple project

---
+++ addBullet.py
@@ -0,0 +1,14 @@
+#! usr/bin/python3
+
+import pyperclip
+
+text = pyperclip.paste()
+
+lines = text.split('.')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+
+text = '\n'.join(lines)
+pyperclip.copy(text)
+
+
